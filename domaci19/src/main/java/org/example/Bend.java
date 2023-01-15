aad97645915de5c8e0a7be6d2ec65f82ac9e43ba
NemanjaAvric <avricnemanja7@gmail.com>
2023-01-15 14:02:04 +0100

    Bend Class added to the file.

---
+++ domaci19/src/main/java/org/example/Bend.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Bend {
+    private String name;
+    private ArrayList<String> bandMembers = new ArrayList<>();
+    private int yearOfFormation;
+
+    public Bend() {
+    }
+}
