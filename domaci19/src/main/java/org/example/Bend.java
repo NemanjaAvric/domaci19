723a0ff271887326e2ec9a3eaf55fd4cb3563377
NemanjaAvric <avricnemanja7@gmail.com>
2023-01-15 14:35:01 +0100

    Getter and Setter methods added to the Band class.

--- domaci19/src/main/java/org/example/Bend.java
+++ domaci19/src/main/java/org/example/Bend.java
@@ -5,8 +5,32 @@ import java.util.ArrayList;
 public class Bend {
     private String name;
     private ArrayList<String> bandMembers = new ArrayList<>();
-    private int yearOfFormation;
+    private int yearOfFormationOfTheBand;
 
     public Bend() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBandMembers(ArrayList<String> bandMembers) {
+        this.bandMembers = bandMembers;
+    }
+
+    public void setYearOfFormation(int yearOfFormation) {
+        this.yearOfFormationOfTheBand = yearOfFormation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getBandMembers() {
+        return bandMembers;
+    }
+
+    public int getYearOfFormation() {
+        return yearOfFormationOfTheBand;
     }
 }
